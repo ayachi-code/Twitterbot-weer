ce4c07cfee70a92e9218c80fa8478359226b9d6a
Bilal ayachi <bilal.ayachi@hetmml.nl>
2018-03-04 14:03:50 +0100

    ignore map

--- authenticatie.js
+++
@@ -1,6 +0,0 @@
-module.exports =  {
-    consumer_key:         'rFRUZNvr7CQeMIO3A5TMEsLhT',
-    consumer_secret:      'eLwJd5dWrhWMZaII3pRBUSC1kEzijUd4BCorWgY0BtdBJU8CsR',
-    access_token:         '970277414986244097-YEPJ81tftlBiCBSa45yGDiC1nJGYHW3',
-    access_token_secret:  'zfmAg5nAmPf0B7Tn2dp1FtepsvLkWYBRrFVjdYjo8qRcU',
-  }
