c3031daeb0be0f7f80dbf462abc0e40ff8889e93
Bilal ayachi <bilal.ayachi@hetmml.nl>
2018-03-04 18:34:29 +0100

    data object bevat graden

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const Twitter_bot = require('twit');
 const fetch = require('fetch').fetchUrl;
-
-let configuratie = require('./autho/authenticatie.js');
-
-let T = new Twitter_bot(configuratie);
+const configuratie = require('./autho/authenticatie.js');
+const T = new Twitter_bot(configuratie);
 
 fetch("http://api.openweathermap.org/data/2.5/weather?q=amsterdam&APPID=78501d5ae347b04883a0f207550d255d&units=metric",(error,info,response) => {
     let begin1 = response.toString()
     let response_json_formaat = JSON.parse(begin1);
+    let data = {
+        graden: response_json_formaat.main.temp
+    }
+    
 });
+
