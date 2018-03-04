733b345e15050e6491f1f2c86e9c87844cda7bca
Bilal ayachi <bilal.ayachi@hetmml.nl>
2018-03-04 15:16:31 +0100

    ik doe een request naar de server

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ const fetch = require('fetch').fetchUrl;
 var configuratie = require('./autho/authenticatie.js');
 
 var T = new Twitter_bot(configuratie);
+
+fetch("http://api.openweathermap.org/data/2.5/weather?q=amsterdam&APPID=78501d5ae347b04883a0f207550d255d&units=metric",(error,info,response) => {
+    
+});
