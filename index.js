9e31fecf72b2142a7d3e1e729c2d5f75a1eb61ee
Bilal ayachi <bilal.ayachi@hetmml.nl>
2018-03-04 15:41:52 +0100

    translate to json formaat

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const Twitter_bot = require('twit');
 const fetch = require('fetch').fetchUrl;
 
-var configuratie = require('./autho/authenticatie.js');
+let configuratie = require('./autho/authenticatie.js');
 
-var T = new Twitter_bot(configuratie);
+let T = new Twitter_bot(configuratie);
 
 fetch("http://api.openweathermap.org/data/2.5/weather?q=amsterdam&APPID=78501d5ae347b04883a0f207550d255d&units=metric",(error,info,response) => {
-    
+    let begin1 = response.toString()
+    let response_json_formaat = JSON.parse(b);
+    console.log(response_json_formaat)
 });
